3c2e550a20887075c27bacf34a844cdd932fd1ee
Radu Lucut <lucutradu@gmail.com>
2019-11-11 18:41:22 +0200

    Add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,63 @@
 const greenToRed = require('../index');
 
-// TODO: Add tests
+describe('greenToRed', () => {
+    test('0% should return RGB', () => {
+        const res = greenToRed(0);
+
+        expect(res.length).toBe(3);
+        expect(res[0]).toBe(0);
+        expect(res[1]).toBe(255);
+        expect(res[2]).toBe(0);
+    });
+
+    test('10% should return HEX', () => {
+        const res = greenToRed(10, true);
+
+        expect(res).toEqual('#32ff00');
+    });
+
+    test('20% should return RGB', () => {
+        const res = greenToRed(20);
+
+        expect(res.length).toBe(3);
+        expect(res[0]).toBe(101);
+        expect(res[1]).toBe(255);
+        expect(res[2]).toBe(0);
+    });
+
+    test('30% should return HEX', () => {
+        const res = greenToRed(30, true);
+
+        expect(res).toEqual('#99ff00');
+    });
+
+    test('50% should return RGB', () => {
+        const res = greenToRed(50);
+
+        expect(res.length).toBe(3);
+        expect(res[0]).toBe(255);
+        expect(res[1]).toBe(255);
+        expect(res[2]).toBe(0);
+    });
+
+    test('70% should return HEX', () => {
+        const res = greenToRed(70, true);
+
+        expect(res).toEqual('#ff9900');
+    });
+
+    test('90% should return RGB', () => {
+        const res = greenToRed(90);
+
+        expect(res.length).toBe(3);
+        expect(res[0]).toBe(255);
+        expect(res[1]).toBe(50);
+        expect(res[2]).toBe(0);
+    });
+
+    test('100% should return HEX', () => {
+        const res = greenToRed(100, true);
+
+        expect(res).toEqual('#ff0000');
+    });
+})
