153a0fb81a728ead60c6e3ac765a07527fe97495
Radu Lucut <lucutradu@gmail.com>
2022-04-03 21:26:30 +0300

    Reduce computations

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const GTR_MAX_VALUE = 255;
+const GTR_FORMULA = 2 / 100 * GTR_MAX_VALUE;
+
 /**
  * 
  * @param {number} percentage 
@@ -5,10 +8,8 @@
  */
 const greenToRed = function (percentage, toHEX) {
     percentage = Math.min(Math.max(0, percentage), 100);
-    const maxValue = 255;
-    const formula = 2 / 100 * maxValue;
-    const green = percentage <= 50 ? maxValue : maxValue - (percentage - 50) * formula;
-    const red = percentage >= 50 ? maxValue : maxValue - (50 - percentage) * formula;
+    const green = percentage <= 50 ? GTR_MAX_VALUE : GTR_MAX_VALUE - (percentage - 50) * GTR_FORMULA;
+    const red = percentage >= 50 ? GTR_MAX_VALUE : GTR_MAX_VALUE - (50 - percentage) * GTR_FORMULA;
     const rgb = new Uint8Array([red, green, 0]);
 
     if (toHEX)
